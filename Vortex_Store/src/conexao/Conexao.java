cb58fc833ee8d80a830ffc2c1ab7084aea3f8cd2
Vinicius Augusto <125403644+ViniciusO6@users.noreply.github.com>
2024-10-04 11:42:56 -0300

    Add Projeto Vortex Store
    Foi adicionado o arquivo do projeto

---
+++ Vortex_Store/src/conexao/Conexao.java
@@ -0,0 +1,66 @@
+package conexao;
+
+import javax.swing.JOptionPane;
+import java.sql.*;
+
+public class Conexao {
+    // Informações do banco de dados
+    final private String driver = "com.mysql.jdbc.Driver";
+    final private String url = "jdbc:mysql://localhost/vortex_store";
+    final private String usuario = "root";
+    final private String senha = "";
+
+    private Connection conexao;
+    public Statement statement;
+    public ResultSet resultset;
+
+    // Método para conectar ao banco de dados
+    //metodo mensagem, se for true exibe a mensagem de conexao estabelecida se for false não exibe nada
+    public boolean conecta(boolean mensagem) {
+        boolean result = true;
+        try {
+            // Carrega o driver JDBC e estabelece a conexão
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            
+            // Cria o statement após estabelecer a conexão
+            statement = conexao.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            if(mensagem == true){
+                JOptionPane.showMessageDialog(null, "Conexão estabelecida", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+        } catch (ClassNotFoundException Driver) {
+            JOptionPane.showMessageDialog(null, "Driver não localizado: " + Driver, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            result = false;
+        } catch (SQLException Fonte) {
+            JOptionPane.showMessageDialog(null, "Fonte de dados não localizada: " + Fonte, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            result = false;
+        }
+        return result;
+    }
+
+    // Método para desconectar do banco de dados
+    public void desconecta() {
+        try {
+            if (conexao != null) {
+                conexao.close();
+                JOptionPane.showMessageDialog(null, "Conexão com o banco fechada", "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } catch (SQLException fecha) {
+            JOptionPane.showMessageDialog(null, "Erro ao fechar a conexão: " + fecha, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    // Método para executar uma consulta SQL
+    public void executaSQL(String sql) {
+        try {
+            if (statement == null) {
+                // Cria o statement se ele não existir
+                statement = conexao.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            }
+            resultset = statement.executeQuery(sql);
+        } catch (SQLException execao) {
+            JOptionPane.showMessageDialog(null, "Erro no comando SQL! \nErro: " + execao, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
